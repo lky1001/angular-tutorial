d8f73e38e8633362ead5d0c34bcf60b1c777d376
kai.1001 <kai.1001@kakaocorp.com>
2017-11-10 18:03:47 +0900

    lang-selector 컴포넌트 생성

---
+++ welcome-msg-app/src/app/lang-selector/lang-selector.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-lang-selector',
+  templateUrl: './lang-selector.component.html',
+  styleUrls: ['./lang-selector.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LangSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
