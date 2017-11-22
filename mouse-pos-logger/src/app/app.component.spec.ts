475b8484c935ded2098a1bcc1f76709587cfc870
kai.1001 <kai.1001@kakaocorp.com>
2017-11-22 14:38:35 +0900

    AppComponent 수정

--- mouse-pos-logger/src/app/app.component.spec.ts
+++ mouse-pos-logger/src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MouseTrackZoneComponent } from './mouse-track-zone/mouse-track-zone.component';
+import { MySpecialLoggerService } from './my-special-logger.service';
+import { AnotherLoggerService } from './another-logger.service';
+import { LOG_LEVEL_TOKEN } from './app.token';
+import { LogLevel } from './log-level.enum';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MouseTrackZoneComponent
       ],
+      providers: [
+        MySpecialLoggerService, 
+        AnotherLoggerService,
+        {
+          provide: LOG_LEVEL_TOKEN,
+          useValue: LogLevel.INFO
+        }
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +28,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'mpl'`, async(() => {
+  it(`should have as title 'mpl works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mpl');
+    expect(app.title).toEqual('mpl works!');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mpl!');
+    expect(compiled.querySelector('h1').textContent).toContain('mpl works!');
   }));
 });
