ab29f0b11397db437b99812e2e84732626ab1a03
kai.1001 <kai.1001@kakaocorp.com>
2017-11-22 15:07:05 +0900

    마우스 트랙 컴포넌트 테스트 코드 추가

--- mouse-pos-logger/src/app/mouse-track-zone/mouse-track-zone.component.spec.ts
+++ mouse-pos-logger/src/app/mouse-track-zone/mouse-track-zone.component.spec.ts
@@ -5,6 +5,7 @@ import { MySpecialLoggerService } from '../my-special-logger.service';
 import { AnotherLoggerService } from '../another-logger.service';
 import { LOG_LEVEL_TOKEN } from '../app.token';
 import { LogLevel } from '../log-level.enum';
+import { By } from '@angular/platform-browser';
 
 describe('MouseTrackZoneComponent', () => {
   let component: MouseTrackZoneComponent;
@@ -32,6 +33,19 @@ describe('MouseTrackZoneComponent', () => {
   });
 
   it('should create', () => {
+    // boolean true 테스트
     expect(component).toBeTruthy();
   });
+
+  it('마우스 클릭스 로그 적재', async(() => {
+    expect(component).toBeTruthy();
+
+    const trackZone = fixture.debugElement.query(By.css('.track-zone'));
+    trackZone.triggerEventHandler("click", { clientX: 1, clientY: 2 });
+    expect((<MySpecialLoggerService>component.logger).logs.length).toEqual(1);
+
+    trackZone.triggerEventHandler("click", { clientX: 5, clientY: 3 });
+    trackZone.triggerEventHandler("click", { clientX: 6, clientY: 4 });
+    expect((<MySpecialLoggerService>component.logger).logs.length).toEqual(3);
+  }));
 });
